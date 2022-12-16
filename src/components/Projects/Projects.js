368e355e3a495c7a6e3af02d18ef4a5c0f1dec4e
tyler <63240641+itstylerrr@users.noreply.github.com>
2022-12-15 21:46:57 -0600

    12-15-22's Hockey Game

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import prioryvwebster from "../../Assets/Photos/121222.JPG";
+import prioryvladue from "../../Assets/Photos/121522.JPG";
 
 function Projects() {
   return (
@@ -23,6 +24,13 @@ function Projects() {
               title="Priory v. Webster 12/12/2022"
               description="W 3-1 | Goals: Freddy Gaertner (x3)"
               ghLink="https://photos.app.goo.gl/STwWGa2h1q4mEjCn7"
+            />
+            <ProjectCard
+              imgPath={prioryvladue}
+              isBlog={false}
+              title="Priory v. Ladue 12/15/2022"
+              description="T 4-4 | Goals: H. Leritz (x1), N. Stutzer (x1), J. Nadreau (x1), F. Gaertner (x1)"
+              ghLink="https://photos.app.goo.gl/oysk8CdAdPCND9PY8"
             />
           </Col>
         </Row>

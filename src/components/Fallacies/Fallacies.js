22db28793f75e4efc3a3086224bb898bfa2e7fe0
tyler <63240641+itstylerrr@users.noreply.github.com>
2023-12-18 21:40:07 -0600

    retry

--- src/components/Fallacies/Fallacies.js
+++ src/components/Fallacies/Fallacies.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
-import Techstack from "./Techstack";
 import Aboutcard from "./FallaciesCard";
-import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
 
 function About() {
   return (
@@ -30,23 +26,16 @@ function About() {
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
-          >
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
+          ></Col>
         </Row>
         <h1 className="project-heading">
           Developing A Professional{" "}
           <strong className="purple">Skillset </strong>
         </h1>
 
-        <Techstack />
-
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
-        <Toolstack />
-
-        <Github />
       </Container>
     </Container>
   );
